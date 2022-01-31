290325b9ba9a7d336b113bdc919a1b266061b087
Fateev <rfney234@gmail.com>
2022-01-31 11:00:02 +0300

    version for local test

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import {initializeApp, geDatabase, ref, set} from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js';
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js';
 import {
     getAuth,
     RecaptchaVerifier,
     signInWithPhoneNumber
 } from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-auth.js';
+import {getDatabase, ref, set} from "https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js";
 
 
 const firebaseConfig = {
@@ -15,55 +16,64 @@ const firebaseConfig = {
     messagingSenderId: "673311742332",
     appId: "1:673311742332:web:9348da767e1994bb452cf9",
     measurementId: "G-MTZTXQYSVW"
-  };
+};
 
 
-initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig);
 
 
 const auth = getAuth();
-auth.languageCode = 'ru';
-window.onload = function () {
-    render();
-}
-
-function render() {
-    window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {}, auth);
-    recaptchaVerifier.render();
-}
-
-// document.getElementById('send').onclick = () => {
-//     signInWithPhoneNumber(auth, '+79507992581', window.recaptchaVerifier).then((confirmationResult) => {
-//         window.confirmationResult = confirmationResult;
-//         console.log(confirmationResult)
-
-//     }).catch(function (error) {
-//         console.log(error);
-//     });
+auth.languageCode = 'Ru';
+// window.onload = function () {
+//     render();
 // }
-document.getElementById('code').onclick = () => {
-    alert('asdads');
+//
+// function render() {
+//     window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {}, auth);
+//     recaptchaVerifier.render();
+//
+// }
+
+document.getElementById('send').onclick = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+        'size': 'invisible',
+        'callback': (response) => {
+            // reCAPTCHA solved, allow signInWithPhoneNumber.
+            onSignInSubmit();
+        }
+    }, auth);
+    const number = document.getElementById('number').value;
+    signInWithPhoneNumber(auth, `+${number}`, window.recaptchaVerifier).then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        console.log(confirmationResult);
+    }).catch((error)  => {
+        console.log(error);
+    });
+}
+document.getElementById('sendDb').onclick = () => {
+
     const code = document.getElementById('verificationCode').value;
     const name = document.getElementById('name').value;
     const lastName = document.getElementById('lastName').value;
     const phone = document.getElementById('number').value;
     const email = document.getElementById('email').value;
-    addMan(name, lastName, phone, email);
-    // confirmationResult.confirm(code).then((result) => {
-        // const user = result.user;
-    // }).catch((error) => {
-
-    // });
+    confirmationResult.confirm(code).then((result) => {
+        const user = result.user;
+        console.log(user)
+        addMan(name, lastName, phone, email);
+    }).catch((error) => {
+        console.log(error);
+    });
 }
 
 function addMan(name, lastName, phone, email) {
-    const db = geDatabase();
-    set(ref(db, '/' ),{
+    const db = getDatabase(app);
+    set(ref(db, '/' + Date.now()), {
         name: name,
         lastName: lastName,
         phone: phone,
         email: email,
-    })
+    }).then(() => alert('user add'))
+        .catch((error) => alert(error));
 }
 
